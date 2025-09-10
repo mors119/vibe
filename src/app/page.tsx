292b415ffd78f3333e51ba7663abaefe368cc77c
heesk0223@gmail.com <heesk0223@gmail.com>
2025-09-10 17:50:31 +0900

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { Button } from '@/components/ui/button';
-
-const Page = () => {
-  return (
-    <div className="font-bold text-rose-500">
-      <Button variant={'link'}>Click me</Button>
-    </div>
-  );
+const Page = async () => {
+  return <div> HELLO</div>;
 };
 
 export default Page;
