4686f5e930135407141b3fa38fb86bb195b2b695
heesk0223@gmail.com <heesk0223@gmail.com>
2025-09-12 11:03:03 +0900

    05: AI Jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,12 +1,22 @@
+import { openai, createAgent } from '@inngest/agent-kit';
+
 import { inngest } from './client';
 
 export const helloWorld = inngest.createFunction(
   { id: 'hello-world' },
   { event: 'test/hello.world' },
-  async ({ event, step }) => {
-    await step.sleep('wait-a-moment', '10s');
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: 'code-agent',
+      system:
+        'You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.',
+      model: openai({ model: 'gpt-4o' }), // api key 이름을 임의 조정했을 경우 model 옆에 추가
+    });
 
-    await step.sleep('wait-a-moment', '5s');
-    return { message: `Hello ${event.data.email}!` };
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`,
+    );
+
+    return { output };
   },
 );
